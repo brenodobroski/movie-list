661c2a3669aba33145dab13f0a269099b3d2b8c6
Breno Dobroski <brenodobroski@gmail.com>
2023-10-02 17:29:04 -0300

    feat: add page movie details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,12 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="Header">
+    <header className="Header">
       <h1>Movie List</h1>
-    </div>
+      <form>
+        <input type="text" placeholder="Pesquise por filmes" />
+      </form>
+    </header>
   );
 };
 
